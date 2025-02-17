9803c0af0f1288244629f62939c25aa3c29ee80a
dotsoulja <c3f1990@gmail.com>
2025-02-16 19:28:48 -0500

    added function to display how many of each character

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 def word_count(text):
     return len(text.split())
 
+def num_of_each_char(text):#creates a dictionary that shows how many of each character there is
+    lower_text = text.lower()
+    char_counts = {}
+
+    for char in lower_text:
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+    return char_counts
+
 
 def main():
     with open("books/Frankenstein.txt") as f: #opens the Frankenstein book
@@ -8,7 +19,11 @@ def main():
     count = word_count(file_contents)
     print(f"The book contains {count} words")
 
-main()
+    letters = num_of_each_char(file_contents)
+    print("the breakdown of how many of each character are as follows:")
+    for char, in sorted(letters.keys()):#prints each key and value on seperate line alphabetically
+        print(f"'{char}': {letters[char]}")
 
+main()
 
 
