12dd6c60edd591c2a2987f347c1c9d12914cb6c8
dotsoulja <c3f1990@gmail.com>
2025-02-16 20:06:48 -0500

    Added function that only includes letters. changed sort to frequency.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def word_count(text):
     return len(text.split())
 
+
 def num_of_each_char(text):#creates a dictionary that shows how many of each character there is
     lower_text = text.lower()
     char_counts = {}
@@ -12,17 +13,35 @@ def num_of_each_char(text):#creates a dictionary that shows how many of each cha
             char_counts[char] = 1
     return char_counts
 
+def sort_on(dict):
+    #helper function for sorting
+    return dict["num"]
+
+def convert_to_list(char_counts):
+    char_list = []
+    for char, count in char_counts.items():
+        if char.isalpha():
+            char_dict = {"char": char, "num": count}
+            char_list.append(char_dict)
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+
 
 def main():
     with open("books/Frankenstein.txt") as f: #opens the Frankenstein book
         file_contents = f.read()  #reads the contents into a string
+
+    print("--- Begin report of books/Frankenstein.txt ---")
     count = word_count(file_contents)
-    print(f"The book contains {count} words")
+    print(f"{count} words found in the document\n")#\n for empty line
 
     letters = num_of_each_char(file_contents)
-    print("the breakdown of how many of each character are as follows:")
-    for char, in sorted(letters.keys()):#prints each key and value on seperate line alphabetically
-        print(f"'{char}': {letters[char]}")
+    char_list = convert_to_list(letters)
+    #loop through char_list
+    for char_dict in char_list:
+        print(f"The '{char_dict['char']}' character was found {char_dict['num']} times")
+    
+    print("--- End report ---")
 
 main()
 
